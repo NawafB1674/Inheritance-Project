f73363990f48bc73cad18170889869e3a51cbf0f
NawafB1674 <nb7535@pleasantonusd.net>
2025-03-26 16:26:54 +0000

    Co-authored-by: AlexK-make <AlexK-make@users.noreply.github.com>

---
+++ src/Facilities.java
@@ -0,0 +1,19 @@
+public class Facilities {
+    
+    private String teamName;
+    private String facilityName;
+
+    public Facilities(String tName, String fName){
+        teamName = tName;
+        facilityName = fName;
+        
+    }
+
+    public String getTeamName(){
+        return "";
+    }
+    
+    public String getFacilityName(){
+        return facilityName;
+    }
+}
