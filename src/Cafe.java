6c168e932fc8cb71cb758453050a6d73f7f4d321
NawafB1674 <nb7535@pleasantonusd.net>
2025-05-12 00:30:11 +0000

    hii

--- src/Cafe.java
+++ src/Cafe.java
@@ -19,4 +19,5 @@ public class Cafe extends Facilities{
 
 
 
+
 }
--- src/Stadium.java
+++ src/Stadium.java
@@ -1,7 +1,18 @@
-public class Stadium extends Facilities{
+public class Stadium extends Facilities {
     private boolean watchingGame = false;
-    private boolean touringtheStadium = false;
+    private boolean touringTheStadium = false;
 
+    public Stadium(String tName, String fName) {
+        super(tName, fName);
+    }
 
-    
+    public void tour() {
+        touringTheStadium = true;
+        System.out.println("You're now touring the beautiful SoFi Stadium!");
+    }
+
+    public void watchGame() {
+        watchingGame = true;
+        System.out.println("You're now watching a live Rams game! Go Rams!");
+    }
 }
